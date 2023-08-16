1cad6798ccc5d6755e482367be205ee15f14deac
belkhiria.hatem <belkhiria.hatem@satoripop.com>
2023-08-16 18:16:34 +0100

    update content II

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -5,15 +5,13 @@ export default function ExperienceCard() {
   return (
     <article className="flex w-[300px] flex-shrink-0 cursor-pointer snap-center flex-col items-center space-y-5 overflow-hidden rounded-lg bg-[#222523] p-8 opacity-40 transition-opacity duration-200 hover:opacity-100 md:w-[600px] xl:w-[900px]">
       <img
-        src={
-          "https://images.crunchbase.com/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/cojcynwhngb2ufvvtvm9"
-        }
+        src={"/sato.webp"}
         alt="Company Logo"
         className="relative h-16 w-16 rounded-full border-[3px] border-[#222523] object-center"
       />
 
       <div className="px-0 md:px-10">
-        <h4 className="text-2xl font-light">Full Stack Developer</h4>
+        <h4 className="text-2xl font-light">Web Developer</h4>
         <p className="mt-1 text-2xl font-bold">SATORIPOP</p>
         <div className="my-2 flex items-center space-x-2">
           <img
@@ -52,7 +50,24 @@ export default function ExperienceCard() {
         </p>
 
         <ul className="ml-5 list-disc space-y-2 text-sm">
-          <li>Getting familliar with bunch of technologies.</li>
+          <li>Developed and maintained responsive web applications.</li>
+          <li>
+            Conducted code reviews and merged pull requests, improving code
+            quality.
+          </li>
+          <li>
+            Proficiently contribute to all stages of the software development
+            life cycle.
+          </li>
+          <li>
+            Leverage expertise in ReactJS to create responsive, visually
+            appealing, and user-friendly frontend interfaces.
+          </li>
+          <li>
+            Actively engage in team dialogues, ideation sessions, and technical
+            parleys, engendering the exchange of insights, resolution of
+            challenges, and the cultivation of a collaborative ethos.
+          </li>
         </ul>
       </div>
     </article>
